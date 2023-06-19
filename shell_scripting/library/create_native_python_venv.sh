ec093050ec42ef3b95e5f34cdd82338b32c33eda
Know Your AI Options <89139139+kyaiooiayk@users.noreply.github.com>
2023-06-19 16:09:21 +0100

    Update and rename create_venv.sh to create_native_python_venv.sh

--- shell_scripting/library/create_native_python_venv.sh
+++ shell_scripting/library/create_native_python_venv.sh
@@ -1,5 +1,5 @@
-python3.8 -m venv .venv
-source .venv/bin/activate
+python3.11 -m venv ~/.venv
+source ~/.venv/bin/activate
 pip3 install --upgrade pip
 pip3 install setuptools==57.1.0
 pip3 install wheel
