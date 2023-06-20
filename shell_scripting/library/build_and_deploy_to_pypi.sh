942479fca214b43c47ee618ed7f5f53f7704b517
Know Your AI Options <89139139+kyaiooiayk@users.noreply.github.com>
2023-06-20 20:06:38 +0100

    Create build_and_deploy_to_pypi.sh

---
+++ shell_scripting/library/build_and_deploy_to_pypi.sh
@@ -0,0 +1,11 @@
+# ./build_deploy.sh` or ./build_deploy.sh --test
+
+rm -r dist ;
+python setup.py sdist bdist_wheel ;
+if twine check dist/* ; then
+  if [ "$1" = "--test" ] ; then
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+  else
+    twine upload dist/* ;
+  fi
+fi
