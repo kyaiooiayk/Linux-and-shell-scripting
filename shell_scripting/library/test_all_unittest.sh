6b3036129fa7a2d2ef21ce51f9b1a5cfac1ad546
Know Your AI Options <89139139+kyaiooiayk@users.noreply.github.com>
2023-06-26 19:05:05 +0100

    Update test_all_unittest.sh

--- shell_scripting/library/test_all_unittest.sh
+++ shell_scripting/library/test_all_unittest.sh
@@ -1,3 +1,6 @@
+# Create coverage
 python -m coverage run -m unittest
+# Create report printed on console
 python -m coverage report
+# Create html; useful to see what you are missing
 python -m coverage html
